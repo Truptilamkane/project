2ffbe6bdae8cf1a30e25959193ad14dc9bfb716f
Your Name <you@example.com>
2022-06-22 22:23:35 +0530

    project

---
+++ Ecommerce/Ecommerce/src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product';
+import { ProductService } from '../services/product.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private _ProductService: ProductService) { }
+
+  products: Array<Product> = new Array<Product>();
+  ngOnInit(): void {
+
+    this._ProductService.getProducts().subscribe(res => this.products = res, err => console.log(err))
+  }
+
+}
